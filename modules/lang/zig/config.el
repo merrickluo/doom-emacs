af35fb4bf91be19b3876f65cec5323fbd2aabd57
A.I <merrick@luois.me>
2022-04-07 11:49:57 +0800

    remove custom zig syntax checker

--- modules/lang/zig/config.el
+++ modules/lang/zig/config.el
@@ -15,15 +15,6 @@
   (when (featurep! +lsp)
     (add-hook 'zig-mode-local-vars-hook #'lsp! 'append))
 
-  (when (featurep! :checkers syntax)
-    (flycheck-define-checker zig
-      "A zig syntax checker using the zig-fmt interpreter."
-      :command ("zig" "fmt" (eval (buffer-file-name)))
-      :error-patterns
-      ((error line-start (file-name) ":" line ":" column ": error: " (message) line-end))
-      :modes zig-mode)
-    (add-to-list 'flycheck-checkers 'zig))
-              
   (map! :localleader
         :map zig-mode-map
         "b" #'zig-compile
